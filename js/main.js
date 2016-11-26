ce930c2f8de21dab4f083ebf5e687f10335f6c54
VitekNek <viktor-nekrasov54@mail.ru>
2016-11-27 00:43:01 +0700

    services pages

--- js/main.js
+++ js/main.js
@@ -150,7 +150,7 @@ $(document).ready(function(){
       event.stopPropagation();
     });
 
-    $('.b-services__slider').slick({
+    $('.page-home .b-services__slider').slick({
         autoplay: true,
         autoplaySpeed: 5000,
         speed: 1000,
@@ -182,50 +182,53 @@ $(document).ready(function(){
     });
 
 
-    var $container = $('#container');
+
 
     // инициализация
-    $('.b-otzivi__container').masonry({
-        columnWidth: 2,
+    $('.b-vacancies .b-otzivi__container').masonry({
+        columnWidth: (isMobile)?1:2,
 
         // обращаемся к пунктам
         itemSelector: '.b-otzivi__cont'
     });
 
 
-    //переопределение высоты блоков товаров
-    //по 3 блока в строке 
+    if (!isMobile)
+    {
+        //переопределение высоты блоков товаров
+        //по 3 блока в строке 
 
-    var maxHeight = 0;
-    var index = 1;
-    var row_height = [];
-    var k = 0;
+        var maxHeight = 0;
+        var index = 1;
+        var row_height = [];
+        var k = 0;
 
-    $(".b-news__cont").each(function(){
-        //ищем максимальную высоту в строке
-        if ( $(this).height() > maxHeight ) 
-            maxHeight = $(this).height();
-        //если прочитали строку
-        //запоминаем максимальную высоту в строке
-        //обнуляем максимум для новой строки
-        if (index%3 == 0) {
-            row_height[k] = maxHeight;
-            k++;
-            maxHeight = 0;
-        }
-        //считаем количество пройденых блоков в строке
-        index++;
-    });
+        $(".b-news__cont").each(function(){
+            //ищем максимальную высоту в строке
+            if ( $(this).height() > maxHeight ) 
+                maxHeight = $(this).height();
+            //если прочитали строку
+            //запоминаем максимальную высоту в строке
+            //обнуляем максимум для новой строки
+            if (index%3 == 0) {
+                row_height[k] = maxHeight;
+                k++;
+                maxHeight = 0;
+            }
+            //считаем количество пройденых блоков в строке
+            index++;
+        });
 
-    //переопределяем высоты
-    index = 1;
-    k=0;
-    $(".b-news__cont").each(function(){
-        $(this).height(row_height[k]);
-        if (index%3 == 0)
-            k++;
-        index++;
-        console.log($(this).height());
-    });
+        //переопределяем высоты
+        index = 1;
+        k=0;
+        $(".b-news__cont").each(function(){
+            $(this).height(row_height[k]);
+            if (index%3 == 0)
+                k++;
+            index++;
+            console.log($(this).height());
+        });
+    }
     
 });
