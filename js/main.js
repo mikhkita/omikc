2ad3d591ce95ab369da523be91bb41ce33a39ddb
VitekNek <viktor-nekrasov54@mail.ru>
2016-12-04 20:34:30 +0600

    mobile menu

--- js/main.js
+++ js/main.js
@@ -232,5 +232,19 @@ $(document).ready(function(){
             console.log($(this).height());
         });
     }
+
+
+    var slideout = new Slideout({
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('menu'),
+        'side': 'right'
+    });
+    document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
+          slideout.toggle();
+        });
+
+        document.querySelector('.menu').addEventListener('click', function(eve) {
+          if (eve.target.nodeName === 'A') { slideout.close(); }
+        });
     
 });
